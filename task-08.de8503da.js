146bc9c1638325576e19d73f9c3774ed22ce5126
MariiaHula <138207508+MariiaHula@users.noreply.github.com>
2023-08-28 14:22:23 +0000

    Deploying to gh-pages from @ MariiaHula/goit-js-hw-06@6aac1a390fbb6a2556a6ac936ae3d34bf70ab1eb 🚀

--- task-08.de8503da.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".login-form");e.addEventListener("submit",(function(l){l.preventDefault();var n=e.elements.email.value,t=e.elements.password.value;if(""===n||""===t)return alert("Please fill in all the fields!");console.log("Login: ".concat(n,", Password: ").concat(t)),e.reset()}))}();
-//# sourceMappingURL=task-08.de8503da.js.map
--- task-08.f1bdd0a5.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".login-form");e.addEventListener("submit",(function(l){l.preventDefault();const t=e.elements.email.value,n=e.elements.password.value;if(""===t||""===n)return alert("Please fill in all the fields!");console.log(`Login: ${t}, Password: ${n}`),e.reset()}));
-//# sourceMappingURL=task-08.f1bdd0a5.js.map
